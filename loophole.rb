58ee76bacf340e20d0cc74c43f188d71e1b5e1e1
Islam Azab <islam.3azab@gmail.com>
2017-08-18 16:33:43 +0200

    refactored loophole model

--- loophole.rb
+++ loophole.rb
@@ -1,8 +1,46 @@
 class Loophole < Source
   def parse
+    node_pairs = get_node_pairs
+
+    routes_hash = JSON.parse(routes_file)
+
+    routes = []
+
+    routes_hash["routes"].each do |route_entry|
+      route = {}
+
+      node_pair_id = route_entry["node_pair_id"]
+      node = node_pairs[node_pair_id]
+      if node
+        route[:start_node] = node[:start_node]
+        route[:end_node]   = node[:end_node]
+
+        route[:start_time] = format_time(route_entry["start_time"])
+        route[:end_time]   = format_time(route_entry["end_time"])
+        routes << route
+      else
+        puts "node_pair_id: #{node_pair_id} doesn't exist!"
+        puts
+      end
+    end
+
+    puts routes
+    puts
+    routes
+  end
+
+  private
+  def routes_file
+    @routes_file ||= File.read("#{@path}/routes.json")
+  end
+
+  def node_pairs_file
+    @node_pairs_file ||= File.read("#{@path}/node_pairs.json")
+  end
+
+  def get_node_pairs
     node_pairs = {}
 
-    node_pairs_file = File.read("#{@path}/node_pairs.json")
     node_pairs_hash = JSON.parse(node_pairs_file)
 
     node_pairs_hash["node_pairs"].each do |node_pair|
@@ -11,35 +49,11 @@ class Loophole < Source
         end_node:   node_pair["end_node"]
       }
     end
+    node_pairs
+  end
 
-    routes_file = File.read("#{@path}/routes.json")
-    routes_hash = JSON.parse(routes_file)
-
-    routes = []
-
-    routes_hash["routes"].each do |route|
-      r = {}
-
-      node_pair_id = route["node_pair_id"]
-      node = node_pairs[node_pair_id]
-      if node
-        r[:start_node] = node[:start_node]
-        r[:end_node]   = node[:end_node]
-
-        r[:start_time] = route["start_time"].gsub("Z","")
-        r[:end_time]   = route["end_time"].gsub("Z","")
-        # r[:start_time] = route["start_time"].strftime('%Y-%m-%dT%H:%M:%S')
-        # r[:end_time]   = route["end_time"].strftime('%Y-%m-%dT%H:%M:%S')
-        routes << r
-      else
-        puts "node_pair_id: #{node_pair_id} doesn't exist!"
-        puts
-      end
-    end
-
-
-    puts routes
-    puts
-    return routes
+  def format_time time
+    # time.strftime('%Y-%m-%dT%H:%M:%S')
+    time.gsub("Z","")
   end
 end
