fec3f90a538815413b5cd18faaa640c68846e6b4
Islam Azab <islam.3azab@gmail.com>
2017-08-18 07:37:55 +0200

    added sniffer class

---
+++ sniffer.rb
@@ -0,0 +1,79 @@
+class Sniffer < Source
+  def parse
+    sequences = {}
+    custom_csv("#{@path}/sequences.csv").each do |row|
+      route_id     = row[0].strip.gsub("\"","")
+      node_time_id = row[1].strip.gsub("\"","")
+      if sequences[route_id]
+        sequences[route_id] << node_time_id
+      else
+        sequences[route_id] = [node_time_id]
+      end
+    end
+
+    puts sequences
+    puts "-"*50
+    puts
+
+    node_times = {}
+    custom_csv("#{@path}/node_times.csv").each do |row|
+      node_time_id             = row[0].strip.gsub("\"","")
+      start_node               = row[1].strip.gsub("\"","")
+      end_node                 = row[2].strip.gsub("\"","")
+      duration_in_milliseconds = row[3].strip.gsub("\"","")
+
+      node_times[node_time_id] = {
+        start_node:               start_node,
+        end_node:                 end_node,
+        duration_in_milliseconds: duration_in_milliseconds
+      }
+    end
+
+    puts node_times
+    puts "-"*50
+    puts
+
+    routes = []
+    custom_csv("#{@path}/routes.csv").each do |row|
+      route_id  = row[0].strip.gsub("\"","")
+      time      = row[1].strip.gsub("\"","")
+      time_zone = row[2].strip.gsub("\"","")
+
+      time       = Time.parse("#{time} #{time_zone}")
+      start_time = time.iso8601.gsub("Z","")
+
+      if sequences[route_id]
+        sequences[route_id].each do |node_time_id|
+          node_time = node_times[node_time_id]
+
+          if node_time
+            r = {}
+            start_node               = node_time[:start_node]
+            end_node                 = node_time[:end_node]
+            duration_in_milliseconds = node_time[:duration_in_milliseconds]
+
+            end_time   = (time + duration_in_milliseconds.to_f/1000).iso8601.gsub("Z","")
+
+            r[:start_node] = start_node
+            r[:end_node]   = end_node
+
+            r[:start_time] = start_time
+            r[:end_time]   = end_time
+            routes << r
+          else
+            puts "node_time_id: #{node_time_id} is missing!"
+          end
+        end
+      else
+        puts "route_id: #{route_id} is missing!"
+      end
+
+    end
+
+    puts routes
+    puts "-"*50
+    puts
+
+    return routes
+  end
+end
